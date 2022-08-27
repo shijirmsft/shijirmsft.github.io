97f953f40d425b8f0b414f2ed9ff531bf6fd6a94
Shijir Tsogoo <shijirtsogoo@microsoft.com>
2022-08-26 18:51:56 -0700

    fix: provide data to share game function

--- js/main.js
+++ js/main.js
@@ -4,11 +4,24 @@ import { questions } from "./questions-inventory.js";
 const shareGameButton = document.querySelector("#share-game");
 const shareGameMSStartButton = document.querySelector("#share-game-msstart");
 
+
+function getShareData () {
+    const url = document.querySelector("#url-control").value;
+    const text = document.querySelector("#text-control").value;
+    const title = document.querySelector("#title-control").value;
+    const context = document.querySelector("#context-control").value;
+
+    return { url, text, title, context };
+}
+
 shareGameButton.addEventListener("click", (event) => {
-    console.log("Clicked on " + event.target.id);
+    const shareData = getShareData();
+    navigator.share(shareData);
+    console.log("Clicked on " + event.target.id, getShareData());
 });
 
 
 shareGameMSStartButton.addEventListener("click", (event) => {
-    console.log("Clicked on " + event.target.id);
+    const shareData = getShareData();
+    console.log("Clicked on " + event.target.id, getShareData());
 });
